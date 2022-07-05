9c6eca3bbcc6fb1711826cc7aff0a58b0579f445
DmitryDobro <dobronovich_95@mail.ru>
2022-07-05 20:28:07 +0300

    burger

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+let burgers = document.querySelectorAll('.header-burger')
+let burger = document.querySelectorAll('.header-burger')
+let menu = document.querySelector('.menu')
+
+for(burger of burgers){
+  burger.addEventListener('click', (event) => {
+    // event.target.classList.toggle('_active') 
+      // burger.classList.toggle('_active') 
+      menu.classList.toggle('_active')
+    
+     
+    document.addEventListener('click', (event) =>{
+        if(!event.target.closest('.menu') && !event.target.closest('.header-burger')){
+            // burger.classList.remove('active') 
+            console.log(123);
+            menu.classList.remove('_active') 
+        }
+    })
+  })
+  
+}
+
+
 const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 4,
